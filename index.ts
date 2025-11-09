d625315809be333dadabfcc0b0837eb7ee1f48ed
cryptogit123 <cryptocal999@gmail.com>
2025-11-09 08:57:05 +0530

    project ini

---
+++ index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send("Hello world 14_be")
+})
+
+app.listen(3000, () => {
+    console.log('Server started at port 3000')
+})
+
+/**
+ *  1. create a docker file to containerize the project
+ *  2. write a ci_cd pipeline to deploy the image to dockerhub and then to vm
+ * 
+ * 
+ */
